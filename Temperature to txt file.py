91d19f411b70aa5a1c8dcd8a8f4acb7816875604
yunjianlu <yunjian@gmail.com>
2019-03-10 00:52:03 -0800

    3/9

--- Temperature to txt file.py
+++ Temperature to txt file.py
@@ -3,9 +3,14 @@ This class is to open an URL, dig into it and finds the usefull data, such as te
 make a copy from it,store them into local txt file
 
 """
+import urllib.request as rq
+import time
+
+
+
 
 class ValiddatingURL:
-    URL = "www.google.com"
+    URL = "http://www.python.org"
 
     def __init__(self, URL):
         if type(URL) is not str:
--- scratch.py
+++ scratch.py
@@ -1,1 +1,43 @@
-print(type("hello"))
+# print(type("hello"))
+#
+#
+# def apply(f, x):
+#    return f(x)
+#
+# apply(lambda x: x * 3, 10)
+#
+# doubles = []
+# l = range(10)
+# for element in l:
+#     doubles.append(element * 2)
+#
+# doubles2 = []
+#
+# doubles2 = list(map(lambda x: x * 2, l))
+# print(doubles2)
+import time
+import time
+import urllib.request as rq
+#
+# url = "http://www.google.com"
+# tem = (str(rq.urlopen(url).read()))
+# for ele in tem:
+#     print (ele)
+
+# print(url, (str(rq.urlopen(url).read())))
+
+
+urls = [
+    'http://www.python.org',
+    'http://www.python.org/about/'
+]
+
+
+def single_fetch():
+   start_time = time.time()
+   list(map(lambda url: print(url, len(str(rq.urlopen(url).read()))), urls))
+   end_time = time.time()
+   print(f"=== threads=1, duration={end_time-start_time} ===")
+
+
+single_fetch()
